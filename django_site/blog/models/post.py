e830abb0aee1df9b91fd7e845a5a164269eb3af5
Denis Oliveira <denisdev3loper@gmail.com>
2024-02-26 00:34:04 -0300

    models

--- django_site/blog/models/post.py
+++ django_site/blog/models/post.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-status = (
+STATUS = (
     (0, 'Draft'),
     (3,'Publish')
 )
 
-class Post(models,Model):
+class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugFields(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     update_on = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
