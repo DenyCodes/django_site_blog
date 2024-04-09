4c5b068ff622f4063985fa8eac5f8cb9ce101b4e
Denis Oliveira <denisdev3loper@gmail.com>
2024-04-09 18:49:17 -0300

    templates

--- django_site/blog/views/post_view.py
+++ django_site/blog/views/post_view.py
@@ -1,9 +1,10 @@
+from django.http import HttpResponse
 from django.views import generic
-from django.models import Post
+from blog.models import Post
 
 
 class PostView(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    queryset = Post.objects.filter(status=1).order_by("-create_on")
     template_name = "index.html"
 
 
